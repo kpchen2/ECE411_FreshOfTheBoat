3c857024460c138adf69ffdc3979c1e9ff543c63
Kevin Chen <kpchen2@illinois.edu>
2024-10-27 19:22:24 -0500

    cleaned up cpu

--- mp_ooo/hdl/cpu.sv
+++ mp_ooo/hdl/cpu.sv
@@ -14,8 +14,13 @@ import rv32i_types::*;
     input   logic   [63:0]      bmem_rdata,
     input   logic               bmem_rvalid
 );
-    logic [31:0] pc;
 
+    logic   [31:0]  pc;
+
+    logic           cache_valid;
+    logic   [255:0] cache_wdata;
+
+    // dfp_rdata and dfp_resp from cacheline adapter
     logic   [31:0]  ufp_addr;
     logic   [3:0]   ufp_rmask;
     logic   [3:0]   ufp_wmask;
@@ -30,6 +35,11 @@ import rv32i_types::*;
     logic   [255:0] dfp_wdata;
     logic           dfp_resp;
 
+    assign ufp_addr = pc;
+    assign ufp_rmask = '1;
+    assign ufp_wmask = '0;
+    assign ufp_wdata = '0;
+
     always_ff @(posedge clk) begin
         if (rst) begin
             pc <= 32'h1eceb000;
@@ -39,13 +49,6 @@ import rv32i_types::*;
             pc <= pc + 4;
         end
     end
-
-    assign ufp_addr = pc;
-    assign ufp_rmask = '1;
-    assign ufp_wmask = '0;
-    assign ufp_wdata = '0;
-
-    // dfp_rdata and dfp_resp from cacheline adapter
 
     fetch fetch_i (
         .*
@@ -68,43 +71,6 @@ import rv32i_types::*;
         .dfp_rdata(dfp_rdata),
         .dfp_wdata(dfp_wdata),
         .dfp_resp(dfp_resp)
-    );
-
-    logic   [31:0]  ufp_addr,
-    logic   [3:0]   ufp_rmask,
-    logic   [3:0]   ufp_wmask,
-    logic   [31:0]  ufp_rdata,
-    logic   [31:0]  ufp_wdata,
-    logic           ufp_resp,
-    logic   [31:0]  dfp_addr,
-    logic           dfp_read,
-    logic           dfp_write,
-    logic   [255:0] dfp_rdata,
-    logic   [255:0] dfp_wdata,
-    logic           dfp_resp
-
-    logic           cache_valid;
-    logic   [255:0] cache_wdata;
-
-    always_ff @(posedge clk) begin
-        // stuff
-    end
-
-    cache cache_i (
-        .clk(clk),
-        .rst(rst),
-        .ufp_addr(),
-        .ufp_rmask(),
-        .ufp_wmask(),
-        .ufp_rdata(),
-        .ufp_wdata(),
-        .ufp_resp(),
-        .dfp_addr(),
-        .dfp_read(),
-        .dfp_write(),
-        .dfp_rdata(),
-        .dfp_wdata(),
-        .dfp_resp()
     );
 
     // outputs cache_valid if cache_wdata is ready
