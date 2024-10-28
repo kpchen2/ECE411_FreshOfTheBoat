53befb0eaec76c9d0e70c10dc6218b18e9f3ecdd
Kevin Chen <kpchen2@illinois.edu>
2024-10-28 13:17:52 -0500

    basic test works fine with dependency_test.s

--- mp_ooo/hdl/cpu.sv
+++ mp_ooo/hdl/cpu.sv
@@ -29,7 +29,7 @@ import rv32i_types::*;
     logic           ufp_resp;
 
     logic   [31:0]  dfp_addr;
-    logic           dfp_read;
+    logic           dfp_read, dfp_read_reg;
     logic           dfp_write;
     logic   [255:0] dfp_rdata;
     logic   [255:0] dfp_wdata;
@@ -38,7 +38,6 @@ import rv32i_types::*;
     logic           initial_flag, initial_flag_reg;     // for initial read AND full_stall reads
     logic           full_stall;
     
-    logic           bmem_ready_dummy;
     logic   [31:0]  bmem_raddr_dummy;
 
     // assign ufp_addr = pc;
@@ -47,16 +46,18 @@ import rv32i_types::*;
     // assign ufp_wdata = '0;
 
     always_ff @(posedge clk) begin
-        bmem_ready_dummy <= bmem_ready;
-        bmem_raddr_dummy <= bmem_raddr;
+        bmem_raddr_dummy <= bmem_raddr; // useless
+        bmem_wdata <= '0;               // useless
 
         if (rst) begin
             pc <= 32'h1eceb000;
             initial_flag_reg <= '1;
+            dfp_read_reg <= '0;
 
         end else begin
             pc <= pc_next;
             initial_flag_reg <= initial_flag;
+            dfp_read_reg <= dfp_read;
         end
     end
 
@@ -65,15 +66,18 @@ import rv32i_types::*;
             pc_next = pc;
             initial_flag = '1;
             ufp_rmask = '0;
+            bmem_read = '0;
 
         end else begin
-            if ((initial_flag_reg || ufp_resp) && !full_stall) begin
+            bmem_read = (!dfp_read_reg && dfp_read) ? '1 : '0;          // bmem_read high on rising dfp_read edge (DOESN'T MATCH TIMING DIAGRAM)
+
+            if ((initial_flag_reg || ufp_resp) && !full_stall && bmem_ready) begin
                 pc_next = pc + 4;
                 initial_flag = '0;
                 ufp_rmask = '1;
 
             end else begin
-                if (full_stall) begin
+                if (full_stall || !bmem_ready) begin
                     pc_next = pc;
                     initial_flag = '1;
                     ufp_rmask = '0;
@@ -106,7 +110,7 @@ import rv32i_types::*;
         .ufp_resp(ufp_resp),
 
         .dfp_addr(bmem_addr),
-        .dfp_read(bmem_read),
+        .dfp_read(dfp_read),
         .dfp_write(bmem_write),
         .dfp_rdata(cache_wdata),
         .dfp_wdata(dfp_wdata),             // FILL WHEN WE WANT TO WRITE
