43ba510c0c7fd2c43cbd8f22e3fbcf36d4017235
nxing2 <nxing2@illinois.edu>
2024-11-01 23:19:41 -0500

    added a freelist

--- mp_ooo/hvl/vcs/fu_multdiv_tb.sv
+++ mp_ooo/hvl/vcs/fu_multdiv_tb.sv
@@ -1,60 +1,60 @@
+// // import rv32i_types::*;
+
+
+// module top_tb;
+
+// timeunit 1ps;
+// timeprecision 1ps;
+// // int clock_half_period_ps = getenv("ECE411_CLOCK_PERIOD_PS").atoi() / 2;
+
 // import rv32i_types::*;
 
+// bit clk;
+// always #1ns clk = ~clk;
 
-module top_tb;
-
-timeunit 1ps;
-timeprecision 1ps;
-// int clock_half_period_ps = getenv("ECE411_CLOCK_PERIOD_PS").atoi() / 2;
-
-import rv32i_types::*;
-
-bit clk;
-always #1ns clk = ~clk;
-
-bit rst;
+// bit rst;
 
 
-initial begin
-    $fsdbDumpfile("dump.fsdb");
-    $fsdbDumpvars(0, "+all");
-    rst = 1'b1;
-    repeat (2) @(posedge clk);
-    rst <= 1'b0;
-end
+// initial begin
+//     $fsdbDumpfile("dump.fsdb");
+//     $fsdbDumpvars(0, "+all");
+//     rst = 1'b1;
+//     repeat (2) @(posedge clk);
+//     rst <= 1'b0;
+// end
 
-    logic   [31:0]  rs1_v, rs2_v;
-    decode_info_t   decode_info;
-    logic   [31:0]  rd_v;
+//     logic   [31:0]  rs1_v, rs2_v;
+//     decode_info_t   decode_info;
+//     logic   [31:0]  rd_v;
 
 
-task generate_reset;
-    begin
-        rst = 1'b1;
-        repeat (2) @ (posedge clk);
-        rst <= 1'b0;
-    end
-endtask;
+// task generate_reset;
+//     begin
+//         rst = 1'b1;
+//         repeat (2) @ (posedge clk);
+//         rst <= 1'b0;
+//     end
+// endtask;
 
-fu_mult_div #(.PHYS_REG_BITS(6)) dut(
-    .*
-);
+// fu_mult_div #(.PHYS_REG_BITS(6)) dut(
+//     .*
+// );
 
-task fu_mult_test;
-    begin
-        decode_info.opcode <= op_b_reg;
-        decode_info.funct3 <= mult_div_f3_mul;
-        rs1_v = 32'h0003;
-        rs2_v = 32'h0008;
-    end
-endtask
+// task fu_mult_test;
+//     begin
+//         decode_info.opcode <= op_b_reg;
+//         decode_info.funct3 <= mult_div_f3_mul;
+//         rs1_v = 32'h0003;
+//         rs2_v = 32'h0008;
+//     end
+// endtask
 
-initial
-begin
-    generate_reset;
-    fu_mult_test;
-    #100000;
-    $finish;
-end
+// initial
+// begin
+//     generate_reset;
+//     fu_mult_test;
+//     #100000;
+//     $finish;
+// end
 
-endmodule
+// endmodule
