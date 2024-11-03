be47219b49cf330a40998f80eebaa5561acbb8b4
Kevin Chen <kpchen2@illinois.edu>
2024-11-03 14:41:03 -0600

    inst queue works

--- mp_ooo/hdl/queue.sv
+++ mp_ooo/hdl/queue.sv
@@ -33,31 +33,30 @@ import rv32i_types::*;
     logic   empty;  // wires, used in sequential logic and in returning output signals
 
     logic   enqueue_reg, enqueue_next;
-    logic   dequeue_reg;
+    logic   dequeue_reg, dequeue_next;
 
     assign  full_out = full;
     assign  empty_out = empty;
 
     always_ff @ (posedge clk) begin
         enqueue_reg <= enqueue_next;
-        dequeue_reg <= dequeue_in;
-        // empty_out <= empty;
+        dequeue_reg <= dequeue_next;
 
         if (rst) begin
-            tail_reg <= '0;
-            head_reg <= '0;
+            tail_reg <= '1;
+            head_reg <= '1;
 
             for (int i = 0; i < QUEUE_DEPTH; i++) begin
                 mem[i] <= '0;
             end
 
         end else begin
-            if (enqueue_in || enqueue_reg) begin
-                mem[tail_reg[$clog2(QUEUE_DEPTH) - 1:0]] <= enqueue_mem_next;
+            if (enqueue_next) begin
+                mem[tail_next[$clog2(QUEUE_DEPTH) - 1:0]] <= enqueue_mem_next;
             end
             
-            if (dequeue_reg) begin
-                mem[head_reg[$clog2(QUEUE_DEPTH) - 1:0]] <= dequeue_mem_next;
+            if (dequeue_next) begin
+                mem[head_next[$clog2(QUEUE_DEPTH) - 1:0]] <= dequeue_mem_next;
             end
 
             tail_reg <= tail_next;
@@ -66,33 +65,31 @@ import rv32i_types::*;
     end
 
     always_comb begin
-        // full = (tail_reg[ADDR_WIDTH - 1:0] == head_reg[ADDR_WIDTH - 1:0]) && (tail_reg[ADDR_WIDTH] != head_reg[ADDR_WIDTH]);    // logic if queue full
-        // empty = (tail_reg[ADDR_WIDTH - 1:0] == head_reg[ADDR_WIDTH - 1:0]) && (tail_reg[ADDR_WIDTH] == head_reg[ADDR_WIDTH ]);  // logic if queue empty
-
         tail_next = tail_reg;
         head_next = head_reg;
         rdata_out = 'x;
         enqueue_mem_next = '0;
         dequeue_mem_next = '0;
         enqueue_next = enqueue_in;
+        dequeue_next = dequeue_in;
 
         if (rst) begin
             full = '0;
             empty = '1;
         end else begin
-            full = (tail_reg[ADDR_WIDTH - 1:0] == head_reg[ADDR_WIDTH - 1:0]) && (tail_reg[ADDR_WIDTH] != head_reg[ADDR_WIDTH]);    // logic if queue full
-            empty = (tail_reg[ADDR_WIDTH - 1:0] == head_reg[ADDR_WIDTH - 1:0]) && (tail_reg[ADDR_WIDTH] == head_reg[ADDR_WIDTH ]);  // logic if queue empty
+            full = (tail_reg[ADDR_WIDTH - 1:0] == head_reg[ADDR_WIDTH - 1:0]) && (tail_reg[ADDR_WIDTH] != head_reg[ADDR_WIDTH]);
+            empty = (tail_reg[ADDR_WIDTH - 1:0] == head_reg[ADDR_WIDTH - 1:0]) && (tail_reg[ADDR_WIDTH] == head_reg[ADDR_WIDTH ]);
         end
 
         if (dequeue_in) begin
             if (~empty) begin   // worry about the valid bit
                 head_next = head_reg + 1'd1;
-                dequeue_mem_next = mem[head_reg[ADDR_WIDTH - 1:0]]; // get current data out of the queue 
-                dequeue_mem_next[DATA_WIDTH] = 1'b0;                // not valid anymore
+                dequeue_mem_next = mem[head_reg[ADDR_WIDTH - 1:0] + 1'b1];  // get current data out of the queue 
+                dequeue_mem_next[DATA_WIDTH] = 1'b0;                        // not valid anymore
                 rdata_out = dequeue_mem_next[DATA_WIDTH - 1:0];
             end else begin
                 head_next = head_reg;
-                dequeue_mem_next = mem[head_reg[ADDR_WIDTH - 1:0]]; // don't do anything
+                dequeue_mem_next = mem[head_reg[ADDR_WIDTH - 1:0]];         // don't do anything
             end
         end
         
