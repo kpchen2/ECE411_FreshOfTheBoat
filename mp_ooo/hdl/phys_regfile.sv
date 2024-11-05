cec1e7a6c178b285d33a050c31d5b985e3561fce
Kevin Chen <kpchen2@illinois.edu>
2024-11-05 13:51:40 -0600

    fixed mul/div rvfi rs1 rs2 value issues

--- mp_ooo/hdl/phys_regfile.sv
+++ mp_ooo/hdl/phys_regfile.sv
@@ -43,15 +43,69 @@ import rv32i_types::*;
     //         rs2_v <= (rs2_s != 5'd0) ? data[rs2_s] : '0;
     //     end
     // end
+
+    logic   [5:0]   rs1_mul_1, rs1_mul_2, rs1_mul_3, rs1_mul_4;
+    logic   [5:0]   rs2_mul_1, rs2_mul_2, rs2_mul_3, rs2_mul_4;
+    logic   [5:0]   rs1_div_1, rs1_div_2, rs1_div_3, rs1_div_4;
+    logic   [5:0]   rs2_div_1, rs2_div_2, rs2_div_3, rs2_div_4;
+
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            rs1_mul_1 <= '0;
+            rs1_mul_2 <= '0;
+            rs1_mul_3 <= '0;
+            rs1_mul_4 <= '0;
+            rs2_mul_1 <= '0;
+            rs2_mul_2 <= '0;
+            rs2_mul_3 <= '0;
+            rs2_mul_4 <= '0;
+
+            rs1_div_1 <= '0;
+            rs1_div_2 <= '0;
+            rs1_div_3 <= '0;
+            rs1_div_4 <= '0;
+            rs2_div_1 <= '0;
+            rs2_div_2 <= '0;
+            rs2_div_3 <= '0;
+            rs2_mul_4 <= '0;
+        end else begin
+            rs1_mul_1 <= rs1_mul;
+            rs1_mul_2 <= rs1_mul_1;
+            rs1_mul_3 <= rs1_mul_2;
+            rs1_mul_4 <= rs1_mul_3;
+            rs2_mul_1 <= rs2_mul;
+            rs2_mul_2 <= rs2_mul_1;
+            rs2_mul_3 <= rs2_mul_2;
+            rs2_mul_4 <= rs2_mul_3;
+
+            rs1_div_1 <= rs1_div;
+            rs1_div_2 <= rs1_div_1;
+            rs1_div_3 <= rs1_div_2;
+            rs1_div_4 <= rs1_div_3;
+            rs2_div_1 <= rs2_div;
+            rs2_div_2 <= rs2_div_1;
+            rs2_div_3 <= rs2_div_2;
+            rs2_div_4 <= rs2_div_3;
+        end
+    end
+
+    logic   [5:0]   rs1_mul_f1, rs2_mul_f2;
+    logic   [5:0]   rs1_div_f1, rs2_div_f2;
+
     always_comb begin
+        rs1_mul_f1 = rst ? '0 : rs1_mul | rs1_mul_1 | rs1_mul_2 | rs1_mul_3 | rs1_mul_4;
+        rs2_mul_f2 = rst ? '0 : rs2_mul | rs2_mul_1 | rs2_mul_2 | rs2_mul_3 | rs2_mul_4;
+        rs1_div_f1 = rst ? '0 : rs1_div | rs1_div_1 | rs1_div_2 | rs1_div_3 | rs1_div_4;
+        rs2_div_f2 = rst ? '0 : rs2_div | rs2_div_1 | rs2_div_2 | rs2_div_3 | rs2_div_4;
+
         rs1_v_add = (rs1_add != 0) ? data[rs1_add] : '0;
         rs2_v_add = (rs2_add != 0) ? data[rs2_add] : '0;
 
-        rs1_v_mul = (rs1_mul != 0) ? data[rs1_mul] : '0;
-        rs2_v_mul = (rs2_mul != 0) ? data[rs2_mul] : '0;
+        rs1_v_mul = data[rs1_mul_f1];
+        rs2_v_mul = data[rs2_mul_f2];
 
-        rs1_v_div = (rs1_div != 0) ? data[rs1_div] : '0;
-        rs2_v_div = (rs2_div != 0) ? data[rs2_div] : '0;
+        rs1_v_div = data[rs1_div_f1];
+        rs2_v_div = data[rs2_div_f2];
     end
 
 endmodule : phys_regfile
